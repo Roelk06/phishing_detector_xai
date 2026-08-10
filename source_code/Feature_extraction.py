f41cf053ca3ae0957e962e518ff2460acb9de929
Roelk06 <r.j.kuipers@tilburguniversity.edu>
2026-08-10 16:40:43 +0200

    expanding feature extraction

--- source_code/Feature_extraction.py
+++ source_code/Feature_extraction.py
@@ -1,9 +1,45 @@
+from pydoc import text
 import re
+import math
+import whois
 import tldextract
+from collections import Counter
+from datetime import datetime
 
 class FeatureExtractor:
     def __init__(self):
         self.email_pattern = re.compile(r"^[\w\.-]+@[\w\.-]+\.\w+$")
+
+    def calculate_entropy(self, input):
+        if not input:
+            return 0.0
+
+        entropy = 0
+        length = len(input)
+        counts = Counter(input)
+
+        for count in counts.values():
+            p = count/length
+            entropy -= p * math.log2(p)
+
+        return float(entropy)
+
+    def calculate_domain_age(self, domain):
+        try:
+            domain_info = whois.whois(domain)
+            creation_date = domain_info.creation_date
+
+            if isinstance(creation_date, list):
+                creation_date = creation_date[0]
+
+            if isinstance(creation_date, datetime):
+                age = (datetime.now() - creation_date).days
+                return age
+
+        except Exception:
+            return -1
+
+        return -1
 
     def extract_features(self, input):
         features = {}
@@ -21,10 +57,23 @@ class FeatureExtractor:
         features["apenstaartje_count"] = input.count("@")
         features["digit_count"] = sum(x.isdigit() for x in input)
 
+        features["domain_entropy"] = self.calculate_entropy(extracted.domain)
+        features["domain_age"] = self.calculate_domain_age(extracted.domain)
+
         return features
 
 #testing
 if __name__ == "__main__":
     extractor = FeatureExtractor()
-    test_mail = "admin@secure-update-apple.com"
-    print("Email Features:", extractor.extract_features(test_mail))
+    
+    # Try a known, old domain
+    test_legit = "admin@tilburguniversity.edu"
+    print("Legit Features:")
+    print(extractor.extract_features(test_legit))
+    
+    print("\n---")
+    
+    # Try a highly random, fake domain
+    test_phish = "http://login-update-xjk89s2f.com/auth"
+    print("Phishing Features:")
+    print(extractor.extract_features(test_phish))
